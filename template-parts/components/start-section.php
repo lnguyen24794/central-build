9edb591eb447dc478d2f355f3204d88bd7419af8
lam.nguyen <lnguyen24794@gmail.com>
2025-10-05 16:58:23 +0700

    Update start-section.php

--- template-parts/components/start-section.php
+++ template-parts/components/start-section.php
@@ -2,7 +2,7 @@
 <section class="about-two-hero-section">
     <div class="w-layout-blockcontainer container-one about-two-hero-container w-container">
         <div class="container position-relative" data-aos="fade-up" data-aos-duration="1200">
-            <h1 class="display-3 fw-bolder mb-3">Commercial Spaces Built for You <br> <span style="color: var(--accent-color);">Simple, Smart</span></h1>
+            <h1 class="display-3 fw-bolder mb-3">Simple, Smart <br> <span style="color: var(--accent-color);">Commercial Spaces Built for You</span></h1>
             <p class="lead mb-4 fw-bold" style="color: var(--accent-color);">CENTRAL BUILD makes fitouts straightforward.</p>
             <p class="mb-5 fs-5 opacity-85 col-lg-8 text-white">with no surprises along the way. We bring our experience and passion into every project, tailoring spaces built with safety, quality, and within budget.</p>
             <a href="#contact" class="btn cta-button-gold btn-lg shadow-lg">START YOUR PROJECT CONSULTATION</a>
