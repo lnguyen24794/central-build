b858545b9bbe2aac402fe0232e8e3dee8791daa1
lam.nguyen <lnguyen24794@gmail.com>
2025-10-05 16:57:22 +0700

    Update start-section.php

--- template-parts/components/start-section.php
+++ template-parts/components/start-section.php
@@ -2,9 +2,9 @@
 <section class="about-two-hero-section">
     <div class="w-layout-blockcontainer container-one about-two-hero-container w-container">
         <div class="container position-relative" data-aos="fade-up" data-aos-duration="1200">
-            <h1 class="display-3 fw-bolder mb-3">COMMERCIAL CONSTRUCTION. <br> <span style="color: var(--accent-color);">RELIABILITY ENGINEERED.</span></h1>
-            <p class="lead mb-4 fw-bold" style="color: var(--accent-color);">We are Central Build: The Vertically Integrated Partner.</p>
-            <p class="mb-5 fs-5 opacity-85 col-lg-8 text-white">Eliminate risk, cut coordination delays, and guarantee compliance with a single, accountable in-house team managing your entire project lifecycle.</p>
+            <h1 class="display-3 fw-bolder mb-3">Commercial Spaces Built for You <br> <span style="color: var(--accent-color);">Simple, Smart</span></h1>
+            <p class="lead mb-4 fw-bold" style="color: var(--accent-color);">CENTRAL BUILD makes fitouts straightforward.</p>
+            <p class="mb-5 fs-5 opacity-85 col-lg-8 text-white">with no surprises along the way. We bring our experience and passion into every project, tailoring spaces built with safety, quality, and within budget.</p>
             <a href="#contact" class="btn cta-button-gold btn-lg shadow-lg">START YOUR PROJECT CONSULTATION</a>
         </div>
     </div>
